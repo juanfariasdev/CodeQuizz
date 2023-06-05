25fd81bed4bb4040a5ec7bc86cda8bc9928c70aa
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-06-04 22:50:05 -0300

    implemented "real" question

--- app/src/main/java/br/edu/ifsuldeminas/codequizz/DashboardActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/codequizz/DashboardActivity.java
@@ -1,6 +1,9 @@
 package br.edu.ifsuldeminas.codequizz;
 
+import static br.edu.ifsuldeminas.codequizz.MainActivity.listOfQuestions;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
 import android.content.Intent;
@@ -10,6 +13,10 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -18,12 +25,29 @@ public class DashboardActivity extends AppCompatActivity {
     int timerValue = 20;
     ProgressBar progressBar;
 
+    List<ModelClass> allQuestionsList;
+
+    ModelClass modelClass;
+
+    int index=0;
+    TextView card_question, option_a,option_b,option_c,option_d;
+    CardView card_option_a,card_option_b,card_option_c,card_option_d;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        progressBar=findViewById(R.id.quiz_timer);
+        Hooks();
+
+
+
+        allQuestionsList=listOfQuestions;
+        Collections.shuffle(allQuestionsList);
+        modelClass=listOfQuestions.get(index);
+
+        setAllData();
 
         countDownTimer=new CountDownTimer(20000, 1000) {
             @Override
@@ -52,5 +76,27 @@ public class DashboardActivity extends AppCompatActivity {
                 dialog.show();
             }
         }.start();
+    }
+
+    private void setAllData() {
+        card_question.setText(modelClass.getQuestion());
+        option_a.setText(modelClass.getoA());
+        option_b.setText(modelClass.getoB());
+        option_c.setText(modelClass.getoC());
+        option_d.setText(modelClass.getoD());
+    }
+
+    private void Hooks() {
+        progressBar=findViewById(R.id.quiz_timer);
+        card_question = findViewById(R.id.quiz_question);
+        option_a = findViewById(R.id.quiz_option_a);
+        option_b = findViewById(R.id.quiz_option_b);
+        option_c = findViewById(R.id.quiz_option_c);
+        option_d = findViewById(R.id.quiz_option_d);
+
+        card_option_a = findViewById(R.id.card_option_a);
+        card_option_b = findViewById(R.id.card_option_b);
+        card_option_c = findViewById(R.id.card_option_c);
+        card_option_d = findViewById(R.id.card_option_d);
     }
 }
--- app/src/main/java/br/edu/ifsuldeminas/codequizz/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/codequizz/MainActivity.java
@@ -6,14 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<ModelClass> listOfQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listOfQuestions = new ArrayList<>();
 
+        listOfQuestions.add(new ModelClass("questaoD","a","b", "c","d", "d"));
+        listOfQuestions.add(new ModelClass("questaoB","a","b", "c","d", "b"));
+        listOfQuestions.add(new ModelClass("questaoA","a","b", "c","d", "a"));
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
